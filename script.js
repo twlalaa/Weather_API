af23875746b5b8f5c6c2dfa656c14c474793ac5c
twlalaa <bloglala2003@gmail.com>
2023-08-22 08:47:59 +0400

    first commit for weather API

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const loading = document.querySelector(".loading");
+
+const city = document.getElementById("city");
+
+const temp = document.querySelector(".temp");
+
+const input = document.querySelector(".search-bar");
+
+const btn = document.getElementById("search-btn");
+
+const weather = document.querySelector(".weather");
+
+const icon = document.querySelector(".icon");
+
+const humidity = document.querySelector(".humidity");
+
+const wind = document.querySelector(".wind");
+
+const API_key = "5c1e801a98de55b805c069472e3bdc3a";
+
+weather.classList.add("hidden");
+
+weather.classList.remove("loading");
+
+const fetchAPI = async (city) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error();
+    }
+    const data = await response.json();
+    console.log(data);
+    displayCity(data);
+    document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${data.name})`;
+    weather.classList.remove("loading");
+  } catch {
+    document.body.style.backgroundImage = "none";
+    weather.classList.add("loading");
+  }
+};
+
+const displayCity = (data) => {
+  weather.classList.remove("hidden");
+  city.innerText = `Weather in ${data.name}`;
+  temp.innerText = `${Math.round(data.main.temp)}°C`;
+  icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  humidity.textContent = `Humidity ${data.main.humidity}%`;
+  wind.textContent = `Wind speed: ${data.wind.speed}km`;
+};
+
+btn.addEventListener("click", () => {
+  const inputValue = input.value.trim();
+  fetchAPI(inputValue);
+  input.value = "";
+});
